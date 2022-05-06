2a36b37227ed2d61c9dc84e2d80024677adb29ba
MMuratttt <muratulusal@yahoo.com>
2022-05-06 20:43:49 +0300

    day2

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkTextPractice.java
@@ -0,0 +1,23 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkTextPractice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://practice.java.com");
+
+
+
+
+    }
+
+
+}
