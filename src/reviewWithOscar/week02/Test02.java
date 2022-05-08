925db992e9f2e9f955f78d4e7341c71e35cdefb2
MMuratttt <muratulusal@yahoo.com>
2022-05-08 12:53:41 +0300

    day 3

---
+++ src/reviewWithOscar/week02/Test02.java
@@ -0,0 +1,66 @@
+package week02;
+
+import com.utilities.HandleWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test02 {
+
+    public static void main(String[] args) {
+
+             //   - Go to:  http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+             //   - Verify title equals: String expectedTitle = "Web Orders Login"
+
+        String expectedTitleBeforeLogin = "Web Orders Login";
+
+        if(expectedTitleBeforeLogin.equals(driver.getTitle())){
+            System.out.println("success");
+        }else{
+            System.exit(1);
+        }
+
+        WebElement userNameBox = driver.findElement(By.id("ctl00_MainContent_username"));
+        WebElement passwordBox = driver.findElement(By.id("ctl00_MainContent_password"));
+        WebElement signInButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+
+        userNameBox.sendKeys("Tester");
+        passwordBox.sendKeys("test");
+        signInButton.click();
+
+        String expectedHomePageTitle = "Web Orders";
+
+        if(expectedHomePageTitle.equals(driver.getTitle())){
+            System.out.println("passed");
+        }else{
+            System.exit(1);
+        }
+
+
+        HandleWait.staticWait(2);
+
+        driver.close();
+
+             //   - Enter username: Tester
+             //   - Enter password: test
+             //   - Click “Sign In” button
+             //   - Verify title equals: String expectedHomePageTitle = "Web Orders"
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/reviewWithOscar/week02/Test03.java
@@ -0,0 +1,51 @@
+package week02;
+
+import com.utilities.HandleWait;
+import com.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Test03 {
+    public static void main(String[] args) {
+
+        // You have to implement the following Web automated checks over our
+        // DEMO ONLINE SHOP: https://www.demoblaze.com/index.html
+    //Customer navigation through product categories: Phones, Laptops and Monitors
+        // Navigate to "Laptop" → "Sony vaio i5" and verify the price is 790 dollars
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.demoblaze.com/index.html");
+
+        HandleWait.staticWait(1);
+
+
+        driver.findElement(By.partialLinkText("Laptops")).click();
+
+        HandleWait.staticWait(1);
+
+
+        driver.findElement(By.linkText("Sony vaio i5")).click();
+
+        HandleWait.staticWait(1);
+
+
+        int expectedPrice = 790;
+
+        String priceText = driver.findElement(By.tagName("h3")).getText();
+
+        int actualPrice = Integer.parseInt(priceText.split(" ")[0].substring(1));
+        System.out.println("actualPrice = " + actualPrice);
+
+        if(expectedPrice == actualPrice){
+            System.out.println("PASSED");
+        }else{
+            System.exit(-1);
+        }
+
+        driver.close();
+
+
+
+    }
+}
