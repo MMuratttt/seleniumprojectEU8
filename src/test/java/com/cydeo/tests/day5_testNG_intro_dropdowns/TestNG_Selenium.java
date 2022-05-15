3a893350848aeafb0ec3e1a34586af2a0dd654a5
MMuratttt <muratulusal@yahoo.com>
2022-05-15 20:58:05 +0300

    day5 testNg

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -0,0 +1,52 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+
+    public void tearDownMethod(){
+
+        driver.close();
+
+    }
+
+
+
+    @Test
+    public void selenium_test(){
+        //Do browser driver setup
+        //Open browswr and google
+        //Assert: title is "Google"
+
+        driver.get("https://www.google.com");
+
+        Assert.assertEquals(driver.getTitle(),"Google","Title is not matching here.");
+
+
+    }
+
+
+
+}
