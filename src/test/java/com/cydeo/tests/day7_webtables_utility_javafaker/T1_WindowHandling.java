99e1e2fc231af35b85c8989198c383e2eb35477b
MMuratttt <muratulusal@yahoo.com>
2022-05-22 15:01:02 +0300

    day7 utilities

--- src/test/java/com/cydeo/tests/day7_webtables_utility_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utility_javafaker/T1_WindowHandling.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day7_webtables_utility_javafaker;
 
+import com.utilities.BrowserUtils;
 import com.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -24,7 +26,7 @@ public class T1_WindowHandling {
     //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
 
     @BeforeMethod
-    public void setupMethod(){
+    public void setupMethod() {
 
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -34,9 +36,33 @@ public class T1_WindowHandling {
 
 
     @Test
-    public void windowHandlingTest(){
+    public void windowHandlingTest() {
 
         driver.get("https://www.amazon.com");
+
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+/*
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+
+            System.out.println("CurrentUrl() = " + driver.getCurrentUrl());
+
+            if(driver.getCurrentUrl().contains("etsy")){
+            break;
+        }}
+
+        String actualTitle = driver.getTitle();
+        String expectedInTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+*/
+
+        BrowserUtils.switchWindowAndVerify(driver,"etsy","Etsy");
+        driver.quit();
 
 
     }
